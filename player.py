61671b368ec8d8d19580b274d59bc77b237b010a
Amarikwa <amarikwa188@gmail.com>
2024-08-27 21:19:02 +0100

    added comments

--- player.py
+++ player.py
@@ -21,19 +21,25 @@ class Bird(Sprite):
         """
         super().__init__()
 
+        # set up screen reference
         self.screen: Surface = screen
         self.screen_rect: Rect = self.screen.get_rect()
 
+        # set the character sprite and create a mask for collisions
         self.image: Surface = pygame.image.load("sprites/circle.png") \
         .convert_alpha()
         self.mask: Mask = pygame.mask.from_surface(self.image)
-        self.rect: Rect = self.image.get_rect()
 
+        # position the character on the screen
+        self.rect: Rect = self.image.get_rect()
         self.rect.centerx = self.screen_rect.centerx
         self.rect.centery = 100
 
+        # a float variable for handling smooth movement
         self.y: float = float(self.rect.centery)
 
+        # add the object to the sprite group and create a reference to the
+        # pipe group
         bird_group.add(self)
         self.pipe_group: Group = pipe_group
 
@@ -46,6 +52,7 @@ class Bird(Sprite):
             s.touching_ceiling = True
             s.last_touched = time.time()
 
+        # apply gravity
         if s.gravity > 0 and self.rect.bottom <= self.screen_rect.bottom:
             self.y += s.gravity
             self.rect.centery = self.y
@@ -53,6 +60,7 @@ class Bird(Sprite):
             self.y += s.gravity
             self.rect.centery = self.y
 
+        # check for collisions
         self.check_collisions()
 
  
@@ -60,7 +68,9 @@ class Bird(Sprite):
         """
         Check for collisions with obstacles.
         """
+        # perform rect collision detection
         if pygame.sprite.spritecollide(self, self.pipe_group, False):
+            # perform sprite collision detection
             if pygame.sprite.spritecollide(self, self.pipe_group, False,
                                            pygame.sprite.collide_mask):
                 sys.exit()
